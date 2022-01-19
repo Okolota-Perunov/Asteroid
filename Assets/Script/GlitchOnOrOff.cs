25ddabe47e5066bcca0e51dfdc1b0f5e0873e6a3
Okolota-Perunov <93677719+Okolota-Perunov@users.noreply.github.com>
2022-01-19 21:07:13 +0300

    Update GlitchOnOrOff.cs

--- Assets/Script/GlitchOnOrOff.cs
+++ Assets/Script/GlitchOnOrOff.cs
@@ -7,10 +7,6 @@ public class GlitchOnOrOff : MonoBehaviour
     [SerializeField] private GameObject Glitch_Go;
     protected float _timerVisibleAlpha;
 
-    protected void Start()
-    {
-
-    }
     protected void Update()
     {
         if(_timerVisibleAlpha <= 0f)
@@ -18,15 +14,17 @@ public class GlitchOnOrOff : MonoBehaviour
             _timerVisibleAlpha = Random.Range(0,100);
         }
         var AlphaGlitch = Glitch_Go.GetComponent<Renderer>().material;
+        
         _timerVisibleAlpha -= Time.deltaTime;
+        
         if(_timerVisibleAlpha <= 30)
         {
             AlphaGlitch.SetFloat("Alpha_",0.2f);
         }
+        
         else
         {
             AlphaGlitch.SetFloat("Alpha_",0f); 
         }
-        
     }
 }
